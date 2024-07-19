77df51ed25774bee52f6c09e28c1a5d8286465ce
Jon Richards <jonr.web@gmail.com>
2024-07-19 09:45:06 +0100

    strip program.cs to bare bones

--- SportTracker.Server/Program.cs
+++ SportTracker.Server/Program.cs
@@ -15,54 +15,56 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder
     .Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents();
+    //.AddInteractiveWebAssemblyComponents();
 
-builder
-    .Services.AddControllersWithViews()
-    .AddJsonOptions(option =>
-    {
-        option.JsonSerializerOptions.Converters.Add(
-            new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
-        );
-    });
+//builder
+//    .Services.AddControllersWithViews()
+//    .AddJsonOptions(option =>
+//    {
+//        option.JsonSerializerOptions.Converters.Add(
+//            new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+//        );
+//    });
 
-builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlite("Filename=../DB/SportTrackerServer.sqlite")
-);
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<ISportEventRepository, SportEventRepository>();
-builder.Services.AddScoped<ISportEventService, SportEventServerService>();
-builder.Services.AddScoped<IUserAuthService, UserAuthServerService>();
-builder.Services.AddScoped<IJwtService, JwtService>();
+//builder.Services.AddDbContext<AppDbContext>(opt =>
+//    opt.UseSqlite("Filename=../DB/SportTrackerServer.sqlite")
+//);
 
-builder.Services.AddCors();
+//builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+//builder.Services.AddScoped<ISportEventRepository, SportEventRepository>();
+//builder.Services.AddScoped<ISportEventService, SportEventServerService>();
+//builder.Services.AddScoped<IUserAuthService, UserAuthServerService>();
+//builder.Services.AddScoped<IJwtService, JwtService>();
 
-builder.Services.AddAuthentication();
-var key = Encoding.ASCII.GetBytes(
-    "50810017cb402db5d4e39d724384cc0dae83fec4b838b796f316a9849ced3639"
-); // fixme get something better
-builder
-    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+//builder.Services.AddCors();
+
+//builder.Services.AddAuthentication();
+//var key = Encoding.ASCII.GetBytes(
+//    "50810017cb402db5d4e39d724384cc0dae83fec4b838b796f316a9849ced3639"
+//); // fixme get something better
+//builder
+//    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey(key),
+//            ValidateIssuer = false,
+//            ValidateAudience = false
+//        };
+//    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 }
-else
+else*/
+if (!app.Environment.IsDevelopment()) 
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -75,13 +77,13 @@ else
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Add).Assembly);
+    .AddInteractiveServerRenderMode();
+    //.AddInteractiveWebAssemblyRenderMode()
+    //.AddAdditionalAssemblies(typeof(Add).Assembly);
 
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
